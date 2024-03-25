a4ad949b7ac21ac1f864c3afb43f3c0bb65a75f2
noel nyakundi <noel.nyakundi@student.moringaschool.com>
2024-03-25 15:31:39 +0300

    array

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// code your solution here
+function superbowlWin(superbowls) {
+    const winningGame = superbowls.find(game => game.result === "W");
+   
+    if (winningGame) {
+        return winningGame.year;
+    } else {
+     
+        return undefined;
+    }
+}
+
+
+const superbowls = [
+    { year: 1999, result: "W" },
+    { year: 2003, result: "L" },
+    { year: 2007, result: "L" },
+    { year: 2011, result: "W" }
+];
+
+console.log(superbowlWin(superbowls)); 
+
+
+
+
+
+
+
+
+
+
+
